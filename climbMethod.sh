d23a564f80c20a5771461bbe3fc2bf3b13078b60
wangjixiao <642907599@qq.com>
2018-10-17 12:43:14 +0800

    update

--- climbMethod.sh
+++ climbMethod.sh
@@ -1,28 +1,25 @@
+#!/bin/bash
+#============== 路径信息 ========================#
 project_path=$(cd `dirname $0`; pwd)
+place_path=${project_path}/placeCode.h
 project_name=WJXAlertView
 folder_path=${project_path}/${project_name}/${project_name}/Class
-place_path=${project_path}/placeCode.h
 
-#读取方法代码
+
+# 遍历文件内容 写入
 function filessScanning {
    path=$1
    cat ${path} | while read line ; do
       if [[ ${line} = +*  ]]; then
-         echo "#define $line $ramdom" >> ${place_path}
+         echo "$line" >> ${place_path}
       fi
       if [[ ${line} = -*  ]]; then
-         if [[ -d ${place_path} ]]; then
-            echo '*******************************  文件已存在  *******************************'
-         else
-            echo '*******************************  创建文件  *******************************'
-            touch ${place_path}
-         fi
-         echo "#define $line $ramdom" >> ${place_path}
+         echo "$line" >> ${place_path}
       fi
    done
 }
 
-# 便利文件
+# 遍历文件
 function convenientFile {
    if [[ -d $1 ]]; then
       for item in $(ls $1); do
@@ -42,7 +39,17 @@ function convenientFile {
    fi
 }
 
+#============== 操作区域 ========================#
+if [[ -f ${place_path} ]]; then
+echo '*******************************  文件已存在  *******************************'
+else
+echo '*******************************  创建文件  *******************************'
+touch ${place_path}
+fi
+
 
 convenientFile ${folder_path}
+sort -u ${place_path} -o ${place_path}
+
 
 
